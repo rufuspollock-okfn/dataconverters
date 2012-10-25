cef540247863285daaf6fd96a0ad8e2eefd316d2
Nigel Babu <nigelbabu@gmail.com>
2012-10-25 18:59:27 +0530

    Change api endpoint

--- manhunter/views.py
+++ manhunter/views.py
@@ -11,13 +11,12 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/convert/<format>')
+@app.route('api/convert/<format>')
 def convert(format=None):
     if (format is None or
-            request.args.get('file') is None or
-            request.args.get('from') is None):
+            request.args.get('url') is None):
         abort(404)
-    r = requests.get(request.args.get('file'))
+    r = requests.get(request.args.get('url'))
     csvcontent = StringIO(r.text)
     csvreader = DictReader(csvcontent)
     dictdata = json.dumps([row for row in csvreader])
