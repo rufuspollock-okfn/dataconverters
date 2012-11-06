b59d7572aec2949b99b238ae3496a12f0f90ce2e
Nigel Babu <nigelbabu@gmail.com>
2012-11-06 21:17:56 +0530

    Header handling greatly improved!

--- manhunter/transform/csv_json_transform.py
+++ manhunter/transform/csv_json_transform.py
@@ -19,7 +19,25 @@ class CSVTransformer(base.Transformer):
         table_set = CSVTableSet.from_fileobj(handle)
         row_set = table_set.tables.pop()
         offset, headers = headers_guess(row_set.sample)
-        row_set.register_processor(headers_processor(headers))
+
+        fields = []
+        dup_columns = {}
+        noname_count = 1
+        for index, field in enumerate(headers):
+            field_dict = {}
+            if "" == field:
+                field = '_'.join(['column', str(noname_count)])
+                headers[index] = field
+                noname_count += 1
+            if headers.count(field) == 1:
+                print field, headers.count(field)
+                field_dict['id'] = field
+            else:
+                print field
+                dup_columns[field] = dup_columns.get(field, 0) + 1
+                field_dict['id'] =  u'_'.join([field, str(dup_columns[field])])
+            fields.append(field_dict)
+        row_set.register_processor(headers_processor([x['id'] for x in fields]))
         row_set.register_processor(offset_processor(offset + 1))
 
         info = {}
@@ -28,6 +46,6 @@ class CSVTransformer(base.Transformer):
             for index, cell in enumerate(row):
                 info[cell.column] = cell.value
             result.append(info)
-        result_data = {'headers': headers, 'data': result}
+        result_data = {'headers': fields, 'data': result}
         result_json = json.dumps(result_data)
         return result_json
