1b189c4f8a31750ccea5817cb5b29a00bbe18c38
Nigel Babu <nigelbabu@gmail.com>
2012-11-23 15:03:30 +0530

    Add type guessing without date guessing

--- manhunter/transform/csv_json_transform.py
+++ manhunter/transform/csv_json_transform.py
@@ -5,7 +5,13 @@ from messytables import (
     CSVTableSet,
     headers_guess,
     headers_processor,
-    offset_processor)
+    offset_processor,
+    type_guess,
+    StringType,
+    IntegerType,
+    FloatType,
+    DecimalType,
+)
 import requests
 import base
 
@@ -19,6 +25,9 @@ class CSVTransformer(base.Transformer):
         table_set = CSVTableSet.from_fileobj(handle)
         row_set = table_set.tables.pop()
         offset, headers = headers_guess(row_set.sample)
+        guess_types = [StringType, IntegerType, FloatType, DecimalType]
+        row_types = type_guess(row_set.sample, guess_types)
+
 
         fields = []
         dup_columns = {}
@@ -34,6 +43,8 @@ class CSVTransformer(base.Transformer):
             else:
                 dup_columns[field] = dup_columns.get(field, 0) + 1
                 field_dict['id'] =  u'_'.join([field, str(dup_columns[field])])
+            field_dict['type'] = str(row_types[index])
+
             fields.append(field_dict)
         row_set.register_processor(headers_processor([x['id'] for x in fields]))
         row_set.register_processor(offset_processor(offset + 1))
