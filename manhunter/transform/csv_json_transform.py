18e2d3ac22130da7a5e62b12d5805f55b8c8c0ac
Nigel Babu <nigelbabu@gmail.com>
2012-11-23 20:49:57 +0530

    Revert "Add type guessing without date guessing"
    This reverts commit 1b189c4f8a31750ccea5817cb5b29a00bbe18c38.

--- manhunter/transform/csv_json_transform.py
+++ manhunter/transform/csv_json_transform.py
@@ -5,13 +5,7 @@ from messytables import (
     CSVTableSet,
     headers_guess,
     headers_processor,
-    offset_processor,
-    type_guess,
-    StringType,
-    IntegerType,
-    FloatType,
-    DecimalType,
-)
+    offset_processor)
 import requests
 import base
 
@@ -25,9 +19,6 @@ class CSVTransformer(base.Transformer):
         table_set = CSVTableSet.from_fileobj(handle)
         row_set = table_set.tables.pop()
         offset, headers = headers_guess(row_set.sample)
-        guess_types = [StringType, IntegerType, FloatType, DecimalType]
-        row_types = type_guess(row_set.sample, guess_types)
-
 
         fields = []
         dup_columns = {}
@@ -43,8 +34,6 @@ class CSVTransformer(base.Transformer):
             else:
                 dup_columns[field] = dup_columns.get(field, 0) + 1
                 field_dict['id'] =  u'_'.join([field, str(dup_columns[field])])
-            field_dict['type'] = str(row_types[index])
-
             fields.append(field_dict)
         row_set.register_processor(headers_processor([x['id'] for x in fields]))
         row_set.register_processor(offset_processor(offset + 1))
