2374d25ea0f8fab34e70c8ad1c62b55dc731072a
Nigel Babu <nigelbabu@gmail.com>
2012-12-19 22:48:01 +0530

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='dataconverters',
+      version='1.0',
+      description='Data Conversion tools for Python',
+      author='Nigel Babu',
+      author_email='nigel.babu@okfn.org',
+      url='https://github.com/okfn/data-converters/',
+      packages=['dataconverters'],
+     )
