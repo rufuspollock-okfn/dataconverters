f09a389d91b04c312f54e735a16a289715213475
Dominik Moritz <domoritz@gmail.com>
2012-12-22 13:51:06 +0100

    [#66] disable type guessing in tests

--- tests/test_csv_json.py
+++ tests/test_csv_json.py
@@ -3,6 +3,7 @@ import datetime
 from nose.tools import assert_equal
 import dataconverters.csv as csvconvert
 
+
 class TestParse:
 
     def setUp(self):
@@ -13,7 +14,7 @@ class TestParse:
     def test_1_convert_csv(self):
         """Test converting a CSV to JSON"""
         csv = open(os.path.join(self.testdata_path, 'simple.csv'))
-        iterator, metadata = csvconvert.parse(csv)
+        iterator, metadata = csvconvert.parse(csv, guess_types=False)
         assert_equal(
             [{'id': u'date'}, {'id': u'temperature'}, {'id': u'place'}],
             metadata['fields'])
@@ -25,7 +26,7 @@ class TestParse:
     def test_2_unicode_csv(self):
         """Test converting a CSV with unicode chars to JSON"""
         csv = open(os.path.join(self.testdata_path, 'spanish_chars.csv'))
-        iterator, metadata = csvconvert.parse(csv)
+        iterator, metadata = csvconvert.parse(csv, guess_types=False)
         assert_equal(
             [{"id": u"GF_ID"}, {"id": u"FN_ID"}, {"id": u"SF_ID"},
                 {"id": u"GF"}, {"id": u"F"}, {"id": u"SF"},
@@ -40,17 +41,15 @@ class TestParse:
                 u"Anexo 11 Cat\u00e1logo Funcional ", u"GF": "", u"FN_ID":
                 u"", u"SF": u""} in content)
 
-
     def test_4_empty_title_convert_csv(self):
         """Test converting a CSV with empty header to JSON"""
         csv = open(os.path.join(self.testdata_path, 'simple_empty_title.csv'))
-        iterator, metadata = csvconvert.parse(csv)
+        iterator, metadata = csvconvert.parse(csv, guess_types=False)
         assert_equal([{"id": u"date"}, {"id": u"column_1"}, {"id":
             u"temperature"}, {"id": u"place"}],
             metadata['fields'])
         content = [row for row in iterator]
         assert ({u"date": u"2011-01-03", u"place": u"Berkeley", u"temperature": u"5", u"column_1": u""} in content)
-
 
     def test_5_header_type(self):
         """Test guessing header type"""
